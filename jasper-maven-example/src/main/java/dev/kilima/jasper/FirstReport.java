c34bb1c136125e3be27d75ed7eec94921dd9036e
John Kilima <jkbundala@gmail.com>
2024-05-23 13:53:09 +0900

    Setting up Java project with Jasper Studio

---
+++ jasper-maven-example/src/main/java/dev/kilima/jasper/FirstReport.java
@@ -0,0 +1,16 @@
+package dev.kilima.jasper;
+
+public class FirstReport {
+
+	public static void main(String[] args) {
+
+
+		try {
+			
+		} catch (Exception e) {
+			System.out.println("Exception while creating reports");
+		}
+
+	}
+
+}
